cb1d803ef75e4a94fd0c08e7c543e3d9441f763b
Simiel Adjei <smenxah@gmail.com>
2025-02-11 16:09:02 +0000

    add auth token to all requests

--- HN-FE/src/main.tsx
+++ HN-FE/src/main.tsx
@@ -3,6 +3,7 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.tsx";
 import { BrowserRouter } from "react-router-dom";
+import { setContext } from "@apollo/client/link/context";
 
 import {
   ApolloProvider,
@@ -10,13 +11,24 @@ import {
   createHttpLink,
   InMemoryCache,
 } from "@apollo/client";
+import { AUTH_TOKEN } from "./constants.ts";
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000",
 });
 
+const authLink = setContext((_, { headers }) => {
+  const token = localStorage.getItem(AUTH_TOKEN);
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : "",
+    },
+  };
+});
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache(),
 });
 
