d49520fc2afd183422993f34671abf518d723cd2
Simiel Adjei <smenxah@gmail.com>
2025-02-11 15:59:09 +0000

    Implemented login, logout and auth state checks

--- HN-FE/src/App.tsx
+++ HN-FE/src/App.tsx
@@ -3,6 +3,7 @@ import CreateLink from "./components/CreateLink";
 import Header from "./components/Header";
 import LinkList from "./components/LinkList";
 import { Route, Routes } from "react-router-dom";
+import Login from "./components/Login";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<LinkList />} />
           <Route path="/create" element={<CreateLink />} />
+          <Route path="/login" element={<Login />} />
         </Routes>
       </div>
     </div>
