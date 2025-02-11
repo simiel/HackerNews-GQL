3a8531fc830960a198f20e8a9bd30cb42474d49e
Simiel Adjei <smenxah@gmail.com>
2025-02-11 15:03:32 +0000

    render links with dummy static data

---
+++ HN-FE/src/components/Link.tsx
@@ -0,0 +1,12 @@
+const Link = (props: any) => {
+  const { link } = props;
+  return (
+    <div>
+      <div>
+        {link.description} ({link.url})
+      </div>
+    </div>
+  );
+};
+
+export default Link;
